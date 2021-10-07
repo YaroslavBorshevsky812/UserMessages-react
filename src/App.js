64d888b714891a947a240b2a3d5006d10da69139
YaroslavBorshevsky812 <Speransky812@mail.ru>
2021-10-08 01:54:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import axios from "axios"
+import { PATH_SMALLDATA, PATH_BIGDATA, PATH_ERRORDATA } from './config/config'
+import { useMemo, useEffect, useState,  } from "react"
+import UserList from './components/userList/UserList'
+import Select from './components/select/Select'
+import Loader from './components/loader/Loader'
+import MyInput from './components/input/MyInput'
+import { useFetching } from "./hooks/useFetching"
+ 
+const App = props => {
+
+  const [userList, setUserList] = useState([])
+  const [selectedSort, setSelectedSort] = useState('')
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [searchQueary, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const [getData, isLoading, userError] = useFetching( async () => { 
+    const response = await axios.get(PATH_SMALLDATA, {
+    })
+    setUserList(response.data)
+  })
+
+  const sortedUsers = useMemo(() => {
+    if(selectedSort) {
+      switch(selectedSort) {
+        case 'по возрастанию':
+          return [...userList].sort((a, b) => b["firstName"].localeCompare(a["firstName"]))
+        case 'по убыванию':
+          return [...userList].sort((a, b) => b["timestamp"].localeCompare(a["timestamp"]))
+      }
+    }
+    return userList
+  }, [selectedSort, userList])
+
+  const sortedAndSearchUsers = useMemo(() => {
+    return sortedUsers.filter(user => user.firstName.includes(searchQueary) || user.lastName.includes(searchQueary))
+  }, [searchQueary, sortedUsers])
+
+  const sortUsers = (sort) => {
+    setSelectedSort(sort)
+  }
+
+    return (
+      <div className="mainPage">
+        <Select
+          value={selectedSort}
+          onChange={sortUsers}
+          defaultValue='Сортировать по дате'
+          options={[
+            {value: 'по убыванию', name: 'по убыванию'},
+            {value: 'по возрастанию', name: 'по возрастанию'}
+          ]}
+        />
+        <MyInput
+          value={searchQueary}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="поиск..."
+        />
+        {userError && 
+          <h2 className="error"> ошибка {userError}</h2>
+        }
+        {
+          isLoading
+                ? <div><Loader/></div>
+                : <UserList loaded={isLoaded} users={sortedAndSearchUsers}/>
+        }
+      </div>
+    ) 
+  
+}
+
+export default App
